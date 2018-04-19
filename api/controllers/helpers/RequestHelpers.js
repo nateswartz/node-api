21b7ab7cee42a07f2923ee3a5cb41e1864fba1e9
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-04-19 13:21:43 -0400

    Add simple, json file caching

--- api/controllers/helpers/RequestHelpers.js
+++ api/controllers/helpers/RequestHelpers.js
@@ -1,17 +1,43 @@
 const axios = require('axios');
+const fs = require('fs');
+const util = require('util');
+
+const existsAsync = util.promisify(fs.exists);
+const statAsync = util.promisify(fs.stat);
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 async function getCollection(url) {
-    let collection = [];
-    try {
-        while (url != null) {
-            const response = await axios.get(url);
-            const data = response.data;
-            url = data.next;
-            collection = collection.concat(data.results);
+    let useCacheFile = false;
+    // Get filesafe name \/:*?"<>|
+    const cacheFile = url.replace(/\/|\\|\:|\*|\?|\"|\<|\>|\|/g, '') + '.json';
+    if (await existsAsync(cacheFile)) {
+        const stats = await statAsync(cacheFile);
+        const modifiedDate = stats.mtime;
+        const elapsedMinutes = Math.floor(((new Date() - modifiedDate) / 1000) / 60);
+        if (elapsedMinutes < 60)
+        {
+            useCacheFile = true;
         }
-        return collection;
-    } catch (error) {
-        return [];
+    }
+    if (useCacheFile) {
+        const cachedResults = JSON.parse(await readFileAsync(cacheFile));
+        return cachedResults;
+    }
+    else {
+        let collection = [];
+        try {
+            while (url != null) {
+                const response = await axios.get(url);
+                const data = response.data;
+                url = data.next;
+                collection = collection.concat(data.results);
+            }
+            await writeFileAsync(cacheFile, JSON.stringify(collection, null, 2), 'utf-8');
+            return collection;
+        } catch (error) {
+            return [];
+        }
     }
 }
 
