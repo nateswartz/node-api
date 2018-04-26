128a382728466ec985be165a701e2c1e4348bb59
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-04-26 08:30:59 -0400

    Consolidate all controllers into a generic controller

---
+++ api/controllers/StarWarsController.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const CollectionResponse = require('../models/CollectionResponse');
+const helpers = require('../helpers/RequestHelpers.js');
+
+exports.getAllItems = async function(req, res) {
+    const items = await helpers.getCollection(req.params.collection);
+    if (items.length != 0) {
+        res.json(new CollectionResponse(items));
+    } else {
+        res.status(404).send('No items found.');
+    }
+};
+
+exports.getItem = async function(req, res) {
+    const item = await helpers.getItem(req.params.collection, req.params.item_id);
+    if (item) {
+        res.json(item);
+    } else {
+        res.status(404).send('No item found.');
+    }
+}
+
+exports.getItemWithInfo = async function(req, res) {
+    const item = await helpers.getItem(req.params.collection, req.params.item_id);
+
+    for (let property in item) {
+        if (Array.isArray(item[property]) || isNaN(item[property]) && (item[property].startsWith('http') && property !== 'url')) {
+            const collection = await helpers.getCollectionForItemProperty(item, property);
+            item[property] = collection;
+        }
+    }
+
+    if (item) {
+        res.json(item);
+    } else {
+        res.status(404).send('No item found.');
+    }
+}
+
+exports.getFilteredItems = async function(req, res) {
+    let items = await helpers.getCollection(req.params.collection);
+    items = helpers.filterCollection(items, req.query);
+    if (items.length != 0) {
+        res.json(new CollectionResponse(items));
+    } else {
+        res.status(404).send('No items found.');
+    }
+};
+
+exports.getRandomItem = async function(req, res) {
+    let items = await helpers.getCollection(req.params.collection);
+    if (items.length !=0) {
+        const randomItem = items[Math.floor(Math.random() * items.length)];
+        res.json(new CollectionResponse(randomItem));
+    } else {
+        res.status(404).send('No items found.');
+    }
+}
--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -1,62 +1,23 @@
 'use strict';
 
+var starWarsController = require('../controllers/StarWarsController');
+
 module.exports = function(app) {
-  var starshipsController = require('../controllers/StarshipsController');
-  var speciesController = require('../controllers/SpeciesController');
-  var peopleController = require('../controllers/PeopleController');
-  var planetsController = require('../controllers/PlanetsController');
 
-    app.route('/starwars/species')
-        .get(speciesController.getAllSpecies);
+    app.route('/starwars/:collection')
+        .get(starWarsController.getAllItems);
 
-    app.route('/starwars/species/filtered')
-        .get(speciesController.getFilteredSpecies);
-
-    app.route('/starwars/species/:species_id')
-        .get(speciesController.getSpecies);
-
-    app.route('/starwars/species/:species_id/all')
-        .get(speciesController.getAllSpeciesInfo);
-
-    app.route('/starwars/starships')
-        .get(starshipsController.getAllStarships);
-
-    app.route('/starwars/starships/:starship_id')
-        .get(starshipsController.getStarship);
-
-    app.route('/starwars/starships/:starship_id/all')
-        .get(starshipsController.getAllStarshipInfo);
+    // Ordering is important, filtered and random must come before item
+    app.route('/starwars/:collection/filtered')
+        .get(starWarsController.getFilteredItems);
     
-    app.route('/starwars/starships/filtered')
-        .get(starshipsController.getFilteredStarships);
-
-    app.route('/starwars/people')
-        .get(peopleController.getAllPeople);
-
-    app.route('/starwars/people/:person_id')
-        .get(peopleController.getPerson);
-
-    app.route('/starwars/people/:person_id/all')
-        .get(peopleController.getAllPersonInfo);
+    app.route('/starwars/:collection/random')
+        .get(starWarsController.getRandomItem);
     
-    app.route('/starwars/people/filtered')
-        .get(peopleController.getFilteredPeople);
+    app.route('/starwars/:collection/:item_id')
+        .get(starWarsController.getItem);
 
-    app.route('/starwars/people/random')
-        .get(peopleController.getRandomPerson);
-
-    app.route('/starwars/planets')
-        .get(planetsController.getAllPlanets);
+    app.route('/starwars/:collection/:item_id/info')
+        .get(starWarsController.getItemWithInfo);
     
-    app.route('/starwars/planets/filtered')
-        .get(planetsController.getFilteredPlanets);
-
-    app.route('/starwars/planets/random')
-        .get(planetsController.getRandomPlanet);
-
-    app.route('/starwars/planets/:planet_id')
-        .get(planetsController.getPlanet);
-
-    app.route('/starwars/planets/:planet_id/all')
-        .get(planetsController.getAllPlanetInfo);
 };
