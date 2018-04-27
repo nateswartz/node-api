fb8cdf5547b1026d1d0b681e9c0f08d53f05efb4
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-04-27 12:33:19 -0400

    Add error handling to ensure controller methods always return

--- api/controllers/StarWarsController.js
+++ api/controllers/StarWarsController.js
@@ -4,63 +4,83 @@ const CollectionResponse = require('../models/CollectionResponse');
 const helpers = require('../helpers/RequestHelpers.js');
 
 exports.getAllItems = async function(req, res) {
-    let items = await helpers.getCollection(req.params.collection);
-    if (req.query.fields) {
-        items = helpers.filterFields(items, req.query.fields);
-    }
-    if (items.length != 0) {
-        res.json(new CollectionResponse(items));
-    } else {
-        res.status(404).send('No items found.');
+    try {
+        let items = await helpers.getCollection(req.params.collection);
+        if (req.query.fields) {
+            items = helpers.filterFields(items, req.query.fields);
+        }
+        if (items.length != 0) {
+            res.json(new CollectionResponse(items));
+        } else {
+            res.status(404).send('No items found.');
+        }
+    } catch (error) {
+        res.status(500).send('Unknown error');
     }
 };
 
 exports.getItem = async function(req, res) {
-    let item = await helpers.getItem(req.params.collection, req.params.item_id);
-    if (req.query.fields) {
-        item = helpers.filterFields(item, req.query.fields);
-    }
-    if (item) {
-        res.json(item);
-    } else {
-        res.status(404).send('No item found.');
+    try {
+        let item = await helpers.getItem(req.params.collection, req.params.item_id);
+        if (req.query.fields) {
+            item = helpers.filterFields(item, req.query.fields);
+        }
+        if (item) {
+            res.json(item);
+        } else {
+            res.status(404).send('No item found.');
+        }
+    } catch (error) {
+        res.status(500).send('Unknown error');
     }
 };
 
 exports.getItemWithInfo = async function(req, res) {
-    let item = await helpers.getItem(req.params.collection, req.params.item_id);
+    try {
+        let item = await helpers.getItem(req.params.collection, req.params.item_id);
 
-    for (let property in item) {
-        if (Array.isArray(item[property])
-            || isNaN(item[property]) && (item[property].startsWith('http') && property !== 'url')) {
-            const collection = await helpers.getCollectionForItemProperty(item, property);
-            item[property] = collection;
+        for (let property in item) {
+            if (Array.isArray(item[property])
+                || (String(item[property]).startsWith('http') && property !== 'url')) {
+                const collection = await helpers.getCollectionForItemProperty(item, property);
+                item[property] = collection;
+            }
         }
-    }
 
-    if (item) {
-        res.json(item);
-    } else {
-        res.status(404).send('No item found.');
+        if (item) {
+            res.json(item);
+        } else {
+            res.status(404).send('No item found.');
+        }
+    } catch (error) {
+        res.status(500).send('Unknown error');
     }
 };
 
 exports.getFilteredItems = async function(req, res) {
-    let items = await helpers.getCollection(req.params.collection);
-    items = helpers.filterCollection(items, req.query);
-    if (items.length != 0) {
-        res.json(new CollectionResponse(items));
-    } else {
-        res.status(404).send('No items found.');
+    try {
+        let items = await helpers.getCollection(req.params.collection);
+        items = helpers.filterCollection(items, req.query);
+        if (items.length != 0) {
+            res.json(new CollectionResponse(items));
+        } else {
+            res.status(404).send('No items found.');
+        }
+    } catch (error) {
+        res.status(500).send('Unknown error');
     }
 };
 
 exports.getRandomItem = async function(req, res) {
-    let items = await helpers.getCollection(req.params.collection);
-    if (items.length !=0) {
-        const randomItem = items[Math.floor(Math.random() * items.length)];
-        res.json(new CollectionResponse(randomItem));
-    } else {
-        res.status(404).send('No items found.');
+    try {
+        let items = await helpers.getCollection(req.params.collection);
+        if (items.length !=0) {
+            const randomItem = items[Math.floor(Math.random() * items.length)];
+            res.json(new CollectionResponse(randomItem));
+        } else {
+            res.status(404).send('No items found.');
+        }
+    } catch (error) {
+        res.status(500).send('Unknown error');
     }
 };
