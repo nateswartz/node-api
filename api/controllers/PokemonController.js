6c49c3bf4fd3e29a331833b0e21b4eba9feac5e1
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-04-28 15:44:19 -0400

    Update PokemonController to use new caching strategy

--- api/controllers/PokemonController.js
+++ api/controllers/PokemonController.js
@@ -1,32 +1,56 @@
 'use strict';
 
+const axios = require('axios');
+const fs = require('fs/promises');
+
 const CollectionResponse = require('../models/CollectionResponse');
-const helpers = require('../helpers/RequestHelpers.js');
 
 const config = require('../../config.json');
 
 exports.getAllItems = async function(req, res) {
     try {
-        let items = await helpers.getCollection(config.pokemonBaseUrl + req.params.collection);
+        const listCacheFileName = './cache/pokemon_list.json';
+        const detailsCacheFileName = './cache/pokemon_details';
+        let items = await getCollection(config.pokemonBaseUrl + req.params.collection,
+                                        listCacheFileName);
         if (req.query.fields) {
-            items = helpers.filterFields(items, req.query.fields);
+            items = filterFields(items, req.query.fields);
         }
-        if (items.length != 0) {
-            res.json(new CollectionResponse(items));
+
+        items = items.slice(0, 30);
+
+        let detailedItems = [];
+        for (let item of items) {
+            const pieces = item.url.split('/');
+            const baseUrl = pieces.slice(0, pieces.length - 2).join('/');
+            const id = pieces[pieces.length - 2];
+            const newItem = await getItem(baseUrl, id, detailsCacheFileName + id + '.json');
+            const cachedResults = await getCachedResults(detailsCacheFileName + id + '.json');
+            if (!cachedResults) {
+                await fs.writeFile(detailsCacheFileName + id + '.json',
+                                   JSON.stringify(newItem, null, 2),
+                                   'utf-8');
+            }
+            detailedItems.push(newItem);
+        }
+
+        if (detailedItems.length != 0) {
+            res.json(new CollectionResponse(detailedItems));
         } else {
             res.status(404).send('No items found.');
         }
     } catch (error) {
-        res.status(500).send('Unknown error');
+        res.status(500).send('Unknown error.');
     }
 };
 
 exports.getItem = async function(req, res) {
     try {
-        let item = await helpers.getItem(config.pokemonBaseUrl + req.params.collection,
-                                         req.params.item_id);
+        const detailsCacheFileName = 'pokemon_details.json';
+        let item = await getItem(config.pokemonBaseUrl + req.params.collection,
+                                 req.params.item_id, detailsCacheFileName);
         if (req.query.fields) {
-            item = helpers.filterFields(item, req.query.fields);
+            item = filterFields(item, req.query.fields);
         }
         if (item) {
             res.json(item);
@@ -88,3 +112,84 @@ exports.getItem = async function(req, res) {
 //         res.status(500).send('Unknown error');
 //     }
 // };
+
+async function getCollection(url, cacheFileName) {
+    const cachedResults = await getCachedResults(cacheFileName);
+    if (cachedResults) {
+        return cachedResults;
+    } else {
+        let collection = [];
+        try {
+            while (url != null) {
+                console.log('Sending request to ' + url);
+                const response = await axios.get(url);
+                const data = response.data;
+                url = data.next;
+                const items = response.data.results;
+                items.forEach(function(item) {
+                    const pieces = item.url.split('/');
+                    const id = pieces[pieces.length - 2];
+                    item.id = id;
+                });
+                collection = collection.concat(data.results);
+            }
+            await fs.writeFile(cacheFileName, JSON.stringify(collection, null, 2), 'utf-8');
+            return collection;
+        } catch (error) {
+            return [];
+        }
+    }
+}
+
+function filterFields(items, fieldList) {
+    if (!Array.isArray(items)) {
+        items = [items];
+    }
+    let fields = fieldList.split(',');
+    let resultList = [];
+    for (let item of items) {
+        let filteredItem = {};
+        for (let field of fields) {
+            if (item[field]) {
+                filteredItem[field] = item[field];
+            }
+        }
+        if (Object.keys(filteredItem).length !== 0) {
+            resultList.push(filteredItem);
+        };
+    }
+    return resultList;
+}
+
+async function getItem(url, id, cacheFileName) {
+    const cachedResults = await getCachedResults(cacheFileName);
+    if (cachedResults) {
+        return cachedResults;
+    } else {
+        try {
+            console.log('Sending request to ' + url + '/' + id);
+            const response = await axios.get(url + '/' + id);
+            response.data.id = id;
+            return response.data;
+        } catch (error) {
+            return null;
+        }
+    }
+}
+
+async function getCachedResults(cacheFileName, ignoreTime) {
+    try {
+        if (ignoreTime) {
+            return JSON.parse(await fs.readFile(cacheFileName));
+        }
+        const stats = await fs.stat(cacheFileName);
+        const modifiedDate = stats.mtime;
+        const elapsedMinutes = Math.floor(((new Date() - modifiedDate) / 1000) / 60);
+        if (elapsedMinutes < config.cacheLifetimeMinutes) {
+            return JSON.parse(await fs.readFile(cacheFileName));
+        }
+        return null;
+    } catch (error) {
+        return null;
+    }
+}
