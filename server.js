d7e02e75b2d4dcf6f41264ec47d8318b765c1c87
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-05-21 20:26:02 -0400

    Add mongo to docker compose, verify connection

--- server.js
+++ server.js
@@ -14,3 +14,40 @@ routes(app); // register the route
 app.listen(port, function() {
     console.log('API server started on: ' + port);
 });
+
+
+// Import the mongoose module
+const mongoose = require('mongoose');
+
+// Set up default mongoose connection
+const mongoDB = 'mongodb://mongo/natetest';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+const SomeModelSchema = new Schema({
+    a_string: String,
+    a_date: Date,
+});
+
+// Compile model from schema
+const SomeModel = mongoose.model('SomeModel', SomeModelSchema );
+
+// Create an instance of model SomeModel
+const awesomeInstance = new SomeModel({ name: 'awesome' });
+
+// Save the new model instance, passing a callback
+awesomeInstance.save(function(err) {
+  if (err) return handleError(err);
+  console.log('Saved!');
+  // saved!
+});
